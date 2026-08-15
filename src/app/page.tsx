552d00c29323af7c908fc885802bbd66ad3a1a26
IBFDENT <kmvfjrp6ps@privaterelay.appleid.com>
2026-08-15 14:45:16 +0200

    commit : zapnutie maintenance obrazovky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,5 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Collections from "@/components/Collections";
-import Materials from "@/components/Materials";
-import Services from "@/components/Services";
-import About from "@/components/About";
-import Boutiques from "@/components/Boutiques";
-import Transfer from "@/components/Transfer";
-import CoppaBeachClub from "@/components/CoppaBeachClub";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import FloatingActions from "@/components/FloatingActions";
+import MaintenanceMode from "@/components/MaintenanceMode";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-
-      <main>
-        <Hero />
-        <Collections />
-        <Materials />
-        <Services />
-        <About />
-        <Boutiques />
-        <Transfer />
-        <CoppaBeachClub />
-        <Contact />
-      </main>
-
-      <Footer />
-      <FloatingActions />
-    </>
-  );
+  return <MaintenanceMode />;
 }
