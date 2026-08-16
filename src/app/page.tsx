5e245c793964322b321476e3265db31e48011d0e
IBFDENT <kmvfjrp6ps@privaterelay.appleid.com>
2026-08-16 22:31:51 +0200

    commit : finalna world class homepage hierarchia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Collections from "@/components/Collections";
-import Materials from "@/components/Materials";
-import Services from "@/components/Services";
-import About from "@/components/About";
-import Boutiques from "@/components/Boutiques";
-import Transfer from "@/components/Transfer";
-import CoppaBeachClub from "@/components/CoppaBeachClub";
+import HomeLuxury from "@/components/HomeLuxury";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import FloatingActions from "@/components/FloatingActions";
@@ -26,13 +20,7 @@ export default function Home() {
 
       <main>
         <Hero />
-        <Collections />
-        <Materials />
-        <Services />
-        <About />
-        <Boutiques />
-        <Transfer />
-        <CoppaBeachClub />
+        <HomeLuxury />
         <Contact />
       </main>
 
