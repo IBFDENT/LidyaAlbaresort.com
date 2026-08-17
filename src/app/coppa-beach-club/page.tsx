56e9ad8276eb4f31d06dd37e8aa7ed561890189f
IBFDENT <kmvfjrp6ps@privaterelay.appleid.com>
2026-08-17 03:01:41 +0200

    commit : obnovena coppa beach club podstranka

---
+++ src/app/coppa-beach-club/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import CoppaBeachClub from "@/components/CoppaBeachClub";
+import Footer from "@/components/Footer";
+import FloatingActions from "@/components/FloatingActions";
+
+export default function CoppaBeachClubPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoppaBeachClub />
+      </main>
+      <Footer />
+      <FloatingActions />
+    </>
+  );
+}
