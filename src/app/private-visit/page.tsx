eed6f5b76bde937aaca726ce63e301af235a5f1b
IBFDENT <kmvfjrp6ps@privaterelay.appleid.com>
2026-08-17 02:42:43 +0200

    commit : obnovena private visit transfer podstranka

---
+++ src/app/private-visit/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Transfer from "@/components/Transfer";
+import Footer from "@/components/Footer";
+import FloatingActions from "@/components/FloatingActions";
+
+export default function PrivateVisitPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Transfer />
+      </main>
+      <Footer />
+      <FloatingActions />
+    </>
+  );
+}
